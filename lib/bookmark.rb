c0a2c8d869712bd746260fe64e0252464a00eeae
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-09-11 15:40:42 +0100

    Ch7 completed: refactor Bookmark.all

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -3,16 +3,9 @@ require 'pg'
 class Bookmark
   def self.all
     conn = PG.connect(dbname: 'bookmark_manager')
-    bookmarks_all_data = []
-    conn.exec( "SELECT * FROM bookmarks" ) do |result|
-      result.each do |row|
-        bookmarks_all_data << row
+    bookmarks_all_data = conn.exec( "SELECT * FROM bookmarks" )
+        bookmarks_all_data.map do |row|
+        row['url']
       end
-      bookmarks_urls = []
-      bookmarks_all_data.each do |row|
-        bookmarks_urls << row["url"]
-      end
-      bookmarks_urls
-    end
   end
 end
